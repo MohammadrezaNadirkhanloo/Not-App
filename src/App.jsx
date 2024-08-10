5fed4a8c7b73a52f7624ed52f675fc3bacc57db2
khanloo <m.nadirkhanloo1380@gmail.com>
2024-08-10 15:00:00 +0330

    Feat : add style header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 function App() {
-  return <>
-<button type="button" class="btn btn-outline-info">Info</button>
-
-  </>;
+  return (
+    <>
+      <div className="row pt-5 pb-3 border-bottom align-items-center">
+        <div className="col-10">
+          <h1 className="text_color m-0">
+            My Note: <span>0</span>
+          </h1>
+        </div>
+        <div data-bs-theme="dark" className="col-2">
+          <select
+            class="form-select bg_input border-0 "
+            aria-label="Default select example"
+          >
+            <option selected value="1">
+              One
+            </option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+      </div>
+      <div></div>
+    </>
+  );
 }
 
 export default App;
