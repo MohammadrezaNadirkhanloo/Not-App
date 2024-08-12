0723094e1e5a6299525416e6fcd1249dd19e9565
khanloo <m.nadirkhanloo1380@gmail.com>
2024-08-12 18:37:19 +0330

    style : status all

--- src/components/ShowStatus.jsx
+++ src/components/ShowStatus.jsx
@@ -1,18 +1,25 @@
-function ShowStatus() {
+function ShowStatus({ datas }) {
+  
   return (
     <div>
       <div className="d-flex justify-content-between">
         <p className="text_color fw-semibold">
           All{" "}
-          <span className="badge text-bg-secondary mx-1 rounded-pill">4</span>
+          <span className="badge text-bg-secondary mx-1 rounded-pill">
+            {datas.length}
+          </span>
         </p>
         <p className="text_color fw-semibold">
           Completed{" "}
-          <span className="badge text-bg-secondary mx-1 rounded-pill">4</span>
+          <span className="badge text-bg-secondary mx-1 rounded-pill">
+            4
+          </span>
         </p>
         <p className="text_color fw-semibold">
           Open{" "}
-          <span className="badge text-bg-secondary mx-1 rounded-pill">4</span>
+          <span className="badge text-bg-secondary mx-1 rounded-pill">
+            0
+          </span>
         </p>
       </div>
     </div>
