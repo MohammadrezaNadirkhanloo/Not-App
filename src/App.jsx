e3bcdf626167483459458bf4fbc533f5cc5236a1
khanloo <m.nadirkhanloo1380@gmail.com>
2024-08-12 10:05:33 +0330

    Fix : fix ui pro

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,13 @@ function App() {
               <label for="Notetitle" className="form-label text_color fw-semibold">
                 Note Title
               </label>
-              <input type="text" className="form-control bg_input border-0 text-white" id="Notetitle" />
+              <input type="text" className="form-control bg_input border-0 text-white" autoComplete="off" id="Notetitle" />
             </div>
             <div className="mb-3">
               <label for="Description" className="form-label text_color fw-semibold">
                 Description:
               </label>
-              <input type="text" className="form-control bg_input border-0 text-white" id="Description" />
+              <input type="text" className="form-control bg_input border-0 text-white" autoComplete="off" id="Description" />
             </div>
             <button type="submit" className="btn btn-primary">
               Add new Note
