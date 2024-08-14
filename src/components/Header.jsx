e11c96bac63a98669f54cddd77e7f01dede848b4
khanloo <m.nadirkhanloo1380@gmail.com>
2024-08-14 21:47:49 +0330

    Feat : add select

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
+
 function Header({ datas }) {
+  const [sort, setSort] = useState("1");
   const allData = datas.length;
+
   return (
     <>
       <div className="col-lg-10">
         <h1 className="text_color m-0">
-          My Note: <span>{allData}</span>
+          My Notes: <span>{allData}</span>
         </h1>
       </div>
       <div data-bs-theme="dark" className="col-lg-2">
         <select
           className="form-select bg_input border-0 cursor"
           aria-label="Default select example"
+          value={sort}
+          onChange={(e) => setSort(e.target.value)}
         >
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          <option value="1">Lastest</option>
+          <option value="2">Earliest</option>
+          <option value="3">Completed</option>
         </select>
       </div>
     </>
